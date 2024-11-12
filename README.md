27124285c6ef01262ec06233d2b66ee1724fefb0
Aidan <21993381+rockie2695@users.noreply.github.com>
2024-11-13 00:59:29 +0800

    test: predict

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ py -3 -m venv .venv
 python -m venv --upgrade venv
 
 .venv\Scripts\activate
+
+pip install -r requirements.txt
+
+python main.py
---
+++ main.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolo11n.pt")  # pretrained YOLO11n model
+
+# Run batched inference on a list of images
+results = model(["image1.jpg", "image2.jpg"])  # return a list of Results objects
+print(results)
+# Process results list
+for idx, result in enumerate(results):
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    obb = result.obb  # Oriented boxes object for OBB outputs
+    print(boxes, masks, keypoints, probs, obb)
+    result.show()  # display to screen
+    result.save(filename="result" + str(idx) + ".jpg")  # save to disk
