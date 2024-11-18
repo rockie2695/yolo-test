e44ad514a311b30d8fb0006e4e3dcee3ef6c8b0d
Aidan <21993381+rockie2695@users.noreply.github.com>
2024-11-19 01:15:04 +0800

    test: object blurring

--- solution/object_cropping.py
+++ solution/object_cropping.py
@@ -5,23 +5,23 @@ import cv2
 from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator, colors
 
-model = YOLO("models/yolo11m.pt")
+model = YOLO("../models/yolo11m.pt")
 names = model.names
 
-cap = cv2.VideoCapture("files/airport_conveyor_belt.mp4")
+cap = cv2.VideoCapture("../files/airport_conveyor_belt.mp4")
 assert cap.isOpened(), "Error reading video file"
 w, h, fps = (
     int(cap.get(x))
     for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS)
 )
 
-crop_dir_name = "ultralytics_crop"
+crop_dir_name = "../video_results/ultralytics_crop"
 if not os.path.exists(crop_dir_name):
     os.mkdir(crop_dir_name)
 
 # Video writer
 video_writer = cv2.VideoWriter(
-    "files/object_cropping_output.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h)
+    "../files/object_cropping_output.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h)
 )
 
 idx = 0
