b0eaca0ac0c26f13c9da1c6a16966a87f0fbe829
Aidan <21993381+rockie2695@users.noreply.github.com>
2024-11-20 01:59:53 +0800

    test

--- solution/object_blurring.py
+++ solution/object_blurring.py
@@ -5,6 +5,9 @@ from ultralytics.utils.plotting import Annotator, colors
 
 model = YOLO("../models/yolo11m.pt")
 names = model.names
+keys = list(names.keys())
+values = list(names.values())
+print(keys[values.index("person")])  # 0
 
 cap = cv2.VideoCapture("../files/airport_conveyor_belt.mp4")
 assert cap.isOpened(), "Error reading video file"
@@ -31,6 +34,7 @@ while cap.isOpened():
 
     results = model.predict(im0, show=False)
     boxes = results[0].boxes.xyxy.cpu().tolist()
+    # boxes = results.boxes.xyxy[results.boxes.cls == person_class_id].cpu().tolist()  # Filter detections to only 'person'
     clss = results[0].boxes.cls.cpu().tolist()
     annotator = Annotator(im0, line_width=2, example=names)
 
